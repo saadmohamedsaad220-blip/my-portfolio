25a0caf5ef14a764c7cc6aba490a8fed194cc503
saadmohamedsaad220-blip <saadmohamedsaad220@gmail.com>
2026-05-08 18:09:39 +0300

    المشروع الأول: إضافة الـ Footer والـ Contact Form

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector(".contact-form");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const name = document.querySelector("#name").value.trim();
+        const email = document.querySelector("#email").value.trim();
+        const password = document.querySelector("#password").value.trim();
+
+        // Validation
+        if (name === "" || email === "" || password === "") {
+            alert("All fields are required!");
+            return;
+        }
+
+        if (!email.endsWith("@azhar.adu.eg")) {
+            alert("Email must end with @azhar.adu.eg");
+            return;
+        }
+
+        if (password.length < 8) {
+            alert("Password must be at least 8 characters");
+            return;
+        }
+
+        // ✅ هنا النجاح → ينقلك لأول صفحة
+        window.location.href = "index1.html#home";
+    });
+    
+
+});
